710a3f683a3752c9a58afab439058dc47f9a52f6
ADreamGift <30544362+ADreamGift@users.noreply.github.com>
2017-08-01 09:21:37 +0800

    add more tab

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
 // Code goes here
 
 
-if('webkitAudioContext' in window) {
-    var myAudioContext = new webkitAudioContext();
-}
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var myAudioContext = new AudioContext();
+
 
 var source = myAudioContext.createOscillator();
+var gainNode = myAudioContext.createGain();
+var finish = myAudioContext.destination;
+
 source.type = 0; // sine wave
 
 source.connect(myAudioContext.destination);
 // play right now (0 seconds from now)
-source.noteOn(0);
-// play 8 notes evenly spaced over the course of a half-second
-for (var i = 0; i < 8; i++) {
-    var rest = i / 16;
-    source.noteOn(myAudioContext.currentTime + rest);
+function playBeat () {
+  var time = arguments[0];
+  source.start(time);
+  source.stop(time + 1/32);
 }
+
+function playBeats (){
+  var num = arguments[0];
+  var currentTime = myAudioContext.currentTime;
+  var step = 1/16;
+  for (var i = 0; i < num ; i++) {
+    source.start(currentTime);
+    source.stop(currentTime + step);
+    currentTime = currentTime + 1;
+  }
+}
+
+$('#myButton').on('click', function () {
+  var $btn = $(this).button('loading')
+  //playBeat();
+  beep();
+
+  // business logic...
+//  $btn.button('reset')
+})
+//playBeats(10)
+//playBeat(0);
+//playBeat(1);
